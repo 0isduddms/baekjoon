f4d2af2e3d2e59b3e565a671bf34b518c57c0cab
0isduddms <dufmaqlw1@naver.com>
2023-02-02 12:27:51 +0900

    프로그래머스 약수의 개수 문제

---
+++ Example/src/programmers/array/MineralWater.java
@@ -0,0 +1,40 @@
+package programmers.array;
+
+public class MineralWater {
+	public static void main(String[] args) {
+		int left = 13;
+		
+		int right = 17;
+		
+		Solution1 sol = new Solution1();
+		
+		int result = sol.solution(left, right);
+		
+		System.out.println(result);
+	}
+}
+class Solution1 {
+    public int solution(int left, int right) {
+        int MW = 0;
+    	int answer = 0;
+    	System.out.println(left/2);
+    	System.out.println((float)left/2);
+    	
+    	for (int i = left; i <= right; i++) {
+    		for (int j = 1; j <= i/2; j++) {
+    			if (i % j == 0) {
+    				MW++;
+    			}
+    		}
+    		if ((MW + 1) % 2 == 0) {
+    			answer += i;
+    		}
+    		else {
+    			answer -= i;
+    		}
+    		MW = 0;
+    	}
+        
+        return answer;
+    }
+}
